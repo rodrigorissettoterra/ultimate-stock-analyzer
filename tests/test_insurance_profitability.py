f15b63086a5a5356a3731dba34da0f8558a6b39e
Rodrigo Terra <prof.rodrigoterra@gmail.com>
2026-08-31 14:20:25 -0300

    Avoid pandas assignment warnings in insurer tests

--- tests/test_insurance_profitability.py
+++ tests/test_insurance_profitability.py
@@ -28,7 +28,6 @@ def test_profitability_uses_december_ytd_average_balances_and_strict_growth() ->
     metrics = derive_susep_profitability_metrics(
         _table(), susep_company_code="12345", fiscal_year=2025
     )
-
     assert metrics.net_income == pytest.approx(20.0)
     assert metrics.roe == pytest.approx(0.20)
     assert metrics.roa == pytest.approx(0.04)
@@ -46,7 +45,6 @@ def test_profitability_allows_negative_net_income_but_growth_fails_closed() -> N
     metrics = derive_susep_profitability_metrics(
         _table(net_income=-10.0), susep_company_code="12345", fiscal_year=2025
     )
-
     assert metrics.roe == pytest.approx(-0.10)
     assert metrics.roa == pytest.approx(-0.02)
     assert metrics.net_income_cagr_5y is None
@@ -55,11 +53,9 @@ def test_profitability_allows_negative_net_income_but_growth_fails_closed() -> N
 def test_growth_requires_all_six_consecutive_december_observations() -> None:
     table = _table()
     table = table.loc[~((table["damesano"] == 202212) & (table["cmpid"] == 518))].copy()
-
     metrics = derive_susep_profitability_metrics(
         table, susep_company_code="12345", fiscal_year=2025
     )
-
     assert metrics.roe == pytest.approx(0.20)
     assert metrics.roa == pytest.approx(0.04)
     assert metrics.net_income_cagr_5y is None
@@ -76,33 +72,22 @@ def test_growth_requires_positive_endpoints_and_nonnegative_intermediate_history
         & (intermediate_table["cmpid"] == 518),
         "valor",
     ] = -1.0
-
-    endpoint_metrics = derive_susep_profitability_metrics(
+    assert derive_susep_profitability_metrics(
         endpoint_table, susep_company_code="12345", fiscal_year=2025
-    )
-    intermediate_metrics = derive_susep_profitability_metrics(
+    ).net_income_cagr_5y is None
+    assert derive_susep_profitability_metrics(
         intermediate_table, susep_company_code="12345", fiscal_year=2025
-    )
-
-    assert endpoint_metrics.net_income_cagr_5y is None
-    assert intermediate_metrics.net_income_cagr_5y is None
+    ).net_income_cagr_5y is None
 
 
 def test_duplicate_net_income_fails_roe_roa_and_growth_closed() -> None:
     table = pd.concat(
-        [
-            _table(),
-            pd.DataFrame(
-                [{"coenti": 12345, "damesano": 202512, "cmpid": 518, "valor": 20.0}]
-            ),
-        ],
+        [_table(), pd.DataFrame([{"coenti": 12345, "damesano": 202512, "cmpid": 518, "valor": 20.0}])],
         ignore_index=True,
     )
-
     metrics = derive_susep_profitability_metrics(
         table, susep_company_code="12345", fiscal_year=2025
     )
-
     assert metrics.net_income is None
     assert metrics.roe is None
     assert metrics.roa is None
@@ -111,14 +96,10 @@ def test_duplicate_net_income_fails_roe_roa_and_growth_closed() -> None:
 
 def test_missing_prior_equity_only_blocks_roe() -> None:
     table = _table()
-    table = table.loc[
-        ~((table["damesano"] == 202412) & (table["cmpid"] == 3333))
-    ].copy()
-
+    table = table.loc[~((table["damesano"] == 202412) & (table["cmpid"] == 3333))].copy()
     metrics = derive_susep_profitability_metrics(
         table, susep_company_code="12345", fiscal_year=2025
     )
-
     assert metrics.roe is None
     assert metrics.roa == pytest.approx(0.04)
     assert metrics.net_income_cagr_5y is not None
@@ -126,45 +107,31 @@ def test_missing_prior_equity_only_blocks_roe() -> None:
 
 def test_missing_prior_assets_only_blocks_roa() -> None:
     table = _table()
-    table = table.loc[
-        ~((table["damesano"] == 202412) & (table["cmpid"] == 1039))
-    ].copy()
-
+    table = table.loc[~((table["damesano"] == 202412) & (table["cmpid"] == 1039))].copy()
     metrics = derive_susep_profitability_metrics(
         table, susep_company_code="12345", fiscal_year=2025
     )
-
     assert metrics.roe == pytest.approx(0.20)
     assert metrics.roa is None
 
 
 def test_nonpositive_average_denominator_fails_closed() -> None:
     table = _table()
-    table.loc[
-        (table["damesano"] == 202512) & (table["cmpid"] == 3333), "valor"
-    ] = -120.0
-    table.loc[
-        (table["damesano"] == 202412) & (table["cmpid"] == 3333), "valor"
-    ] = 80.0
-
+    table.loc[(table["damesano"] == 202512) & (table["cmpid"] == 3333), "valor"] = -120.0
+    table.loc[(table["damesano"] == 202412) & (table["cmpid"] == 3333), "valor"] = 80.0
     metrics = derive_susep_profitability_metrics(
         table, susep_company_code="12345", fiscal_year=2025
     )
-
     assert metrics.roe is None
     assert metrics.roa == pytest.approx(0.04)
 
 
 def test_nonnumeric_value_fails_only_dependent_metric() -> None:
     table = _table().astype({"valor": "object"})
-    table.loc[
-        (table["damesano"] == 202512) & (table["cmpid"] == 1039), "valor"
-    ] = "UNKNOWN"
-
+    table.loc[(table["damesano"] == 202512) & (table["cmpid"] == 1039), "valor"] = "UNKNOWN"
     metrics = derive_susep_profitability_metrics(
         table, susep_company_code="12345", fiscal_year=2025
     )
-
     assert metrics.roe == pytest.approx(0.20)
     assert metrics.roa is None
 
@@ -176,7 +143,6 @@ def test_wrong_company_and_pre_contract_year_are_unknown() -> None:
     old_year = derive_susep_profitability_metrics(
         _table(), susep_company_code="12345", fiscal_year=2013
     )
-
     assert wrong_company.roe is None
     assert wrong_company.roa is None
     assert wrong_company.net_income_cagr_5y is None
@@ -187,9 +153,7 @@ def test_wrong_company_and_pre_contract_year_are_unknown() -> None:
 
 def test_company_code_must_be_numeric() -> None:
     with pytest.raises(ValueError, match="only digits"):
-        derive_susep_profitability_metrics(
-            _table(), susep_company_code="ABC", fiscal_year=2025
-        )
+        derive_susep_profitability_metrics(_table(), susep_company_code="ABC", fiscal_year=2025)
 
 
 def test_required_schema_is_enforced() -> None:
